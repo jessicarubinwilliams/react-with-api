2ed1c55613d39241a5a2260a0d3a8660c6197cdb
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2022-02-13 13:02:33 -0800

    Add code to pass test of action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,15 @@
 import * as constants from './ActionTypes';
 
 export const requestHeadlines = () => ({
- 
+  type: constants.REQUEST_HEADLINES
 });
 
 export const getHeadlinesSuccess = (headlines) => ({
-  
+  type: constants.GET_HEADLINES_SUCCESS,
+  headlines
 });
 
 export const getHeadlinesFailure = (error) => ({
-  
+  type: constants.GET_HEADLINES_FAILURE,
+  error
 });
