0771357c75a7c4aeae2c2b249859f0ec89c19042
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2022-02-13 12:29:56 -0800

    Add code to pass test of headlinesReducer changing isLoading state slice from false to true when headlines are requested from the API.

--- src/reducers/headlines-reducer.js
+++ src/reducers/headlines-reducer.js
@@ -1,9 +1,18 @@
-const defaultState = {
+import * as constants from './../actions/ActionTypes'
+
+const initialState = {
   isLoading: false,
   headlines: [],
   error: null
 }
 
-export default (state = defaultState, action) => {
-  return state;
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case constants.REQUEST_HEADLINES:
+      return Object.assign({}, state, {
+        isLoading: true
+      });
+    default:
+      return state;
+  }
 };
